7e9d6159fd7c2eb89384ff4bbf1c75798bfbd046
Redari-Es <905422030@qq.com>
2023-12-19 17:08:00 +0800

    Part 1: Prototype & simple implement

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+// 设置hash值
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, PrevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func main() {
+	bc := NewBlockchain()
+	bc.AddBlock("Send 1 BTC to Shon")
+	bc.AddBlock("Send 2 more BTC to Shon")
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+
+	}
+}
